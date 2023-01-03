da8616a6fe18d3bcdd4f4f29a02bfadf6ea3e5ce
Wieger Wesselink <j.w.wesselink@tue.nl>
2023-01-04 00:33:25 +0100

    Use draughts notation in piece animation example

--- src/cm-draughtsboard/DraughtsBoard.js
+++ src/cm-draughtsboard/DraughtsBoard.js
@@ -6,7 +6,7 @@
  */
 
 import {Board} from "./Board.js"
-import {DraughtsBoardState} from "./DraughtsBoardState.js";
+import {DraughtsBoardState} from "./DraughtsBoardState.js"
 
 export class DraughtsBoard extends Board {
 
@@ -15,5 +15,35 @@ export class DraughtsBoard extends Board {
             props.state = new DraughtsBoardState(props.position)
         }
         super(context, props)
+    }
+
+    isNonPlayingField(field) {
+        let [r, c] = this.state.f2rc(field)
+        return this.state.isNonPlayingField(r, c)
+    }
+
+    setPiece(field, piece, animated = false) {
+        if (this.isNonPlayingField(field)) {
+            return
+        }
+        let index = this.state.f2index(field)
+        return super.setPiece(index, piece, animated)
+    }
+
+    getPiece(field) {
+        if (this.isNonPlayingField(field)) {
+            return
+        }
+        let index = this.state.f2index(field)
+        return super.getPiece(index)
+    }
+
+    movePiece(fieldFrom, fieldTo, animated = false) {
+        if (this.isNonPlayingField(fieldFrom) || this.isNonPlayingField(fieldTo)) {
+            return
+        }
+        let indexFrom = this.state.f2index(fieldFrom)
+        let indexTo = this.state.f2index(fieldTo)
+        return super.movePiece(indexFrom, indexTo, animated)
     }
 }
