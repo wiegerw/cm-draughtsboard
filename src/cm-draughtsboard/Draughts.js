b5b74cf259972fcedf82a3b6fc2818fbe8b6f10c
Wieger Wesselink <j.w.wesselink@tue.nl>
2023-01-03 18:22:16 +0100

    Added Draughts.js
    - For convenience, the most common imports are put in this file

---
+++ src/cm-draughtsboard/Draughts.js
@@ -0,0 +1,10 @@
+/**
+ * Author and copyright: Wieger Wesselink (https://10x10.org)
+ * Repository: https://github.com/wiegerw/cm-draughtsboard
+ * License: MIT, see file 'LICENSE'
+ */
+
+import {BORDER_TYPE, COLOR, INPUT_EVENT_TYPE} from "./Board.js"
+import {DraughtsBoard} from "./DraughtsBoard.js"
+import {DRAUGHTS} from "./DraughtsBoardState.js"
+export {BORDER_TYPE, COLOR, INPUT_EVENT_TYPE, DRAUGHTS, DraughtsBoard}
